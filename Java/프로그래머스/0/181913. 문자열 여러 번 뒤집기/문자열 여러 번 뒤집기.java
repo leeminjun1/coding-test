ade0e83e2a0e8ee987551905b04541eda42a03e6
이민준 <lmjdooda@gmail.com>
2026-07-26 23:43:40 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.56 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String my_string, int[][] queries) {
+        char[] chars = my_string.toCharArray();
+
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            while (start < end) {
+                char temp = chars[start];
+                chars[start] = chars[end];
+                chars[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        return new String(chars);
+    }
+}
